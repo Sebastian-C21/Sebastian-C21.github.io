fa43b69829083683110e852f90172d5357a88798
Sebastian-C21 <sebastian981211@gmail.com>
2020-06-28 18:36:06 -0500

    5W: DOM manipulation

--- script.js
+++
@@ -1,9 +0,0 @@
-sebGreeter.sayHi();
-juanGreeter.sayHi();
-
-//immediately invoked functions expression. (IIFE)
-//It allows the code inside the function don't conflict with the global code
-console.log("** Example of IIFE below");
-(function (namee) {
-    console.log("I love "+namee);
-})("Pixels");
--- script_2.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-    var sebGreeter = {};
-    sebGreeter.name = "Sebastian";
-    var greeting = "Hi! ";
-    sebGreeter.sayHi = function () {
-        console.log(greeting+sebGreeter.name);
-    }
-    window.sebGreeter = sebGreeter;
-})(window);
