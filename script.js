93ef3615cf31a22c8d19830dffd2847fc50e2fd1
Sebastian-C21 <sebastian981211@gmail.com>
2020-07-01 18:10:55 -0500

    5W: Menu button fix with JS
    Collapse is an JQuery plugin so the function used is $

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-//Event handling
-document.addEventListener("DOMContentLoaded",
-    function (event) {
-
-        //Unobstrusive event binding
-        document.querySelector("button")
-            .addEventListener("click", function() {
-
-                //Call server to get the name
-                $ajaxUtils
-                    .sendGetRequest("/5_W/name.json",
-                        function(res) {
-                            var message = "Hello " + res.firstName + " " +
-                                res.lastName + " i know you ";
-                                if (res.likesChineseFood) {
-                                    message += "like Chinese Food! and ";
-                                }else {
-                                    message += "don't like Chinese food! and ";
-                                }
-                                message += "that you have " + res.numberOfDisplays +
-                                    " displays to code! keep it up!";
-                            document.querySelector("#content")
-                                .innerHTML = "<h2>"+  message + "</h2>";
-                        });
-            });
-    }
-);
+//Event handling:JQuery function name is $
+$(function (){ //same as document.addEventListener("DOMContentLoaded, ...")
+    //same as document.querySelector("#navbarToggle").addEventListener("blur", ...)
+    $("#navbarToggle").blur(function(event){
+        var screenWidth = window.innerWidth;
+        if (screenWidth < 768) {
+            $("#collapsable-nav").collapse('hide');
+        }
+    });
+});
